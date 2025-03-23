53780a71a3104fcd66dbd0d9121c4d76707d1332
Ajay <jaykumar002kori@gmail.com>
2025-03-23 12:33:33 +0530

    Add database management module and dotenv support

---
+++ Database/Manage_database.py
@@ -0,0 +1,35 @@
+import os 
+import sys
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+### confing mongodb
+
+class Manage_db:
+    
+    def __init__(self, db_name:str):
+        load_dotenv()
+        self.__db_name  = db_name
+        self.__db_uri = os.getenv("DB_URI")
+        self.__client = MongoClient(self.__db_uri, server_api= ServerApi('1'))   
+              
+    def get_db(self):
+        """ Get the database object """
+        try:
+            client = self.__client
+            db = client[self.__db_name]
+            return db
+        except Exception as e:
+            print(e)
+            
+    def close_connection(self):
+        """ Close the connection """
+        try:
+            self.__client.close()
+            return None
+        except Exception as e:
+            print(e)
+                
+
+    
