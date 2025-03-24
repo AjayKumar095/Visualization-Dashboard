e0ca5d8f4ab2d659a546250a9875a78e95a04a25
Ajay <jaykumar002kori@gmail.com>
2025-03-24 19:02:26 +0530

    Add API endpoint to retrieve data from the database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, jsonify
 from Database.Manage_database import Manage_db
 
 
@@ -11,6 +11,19 @@ def index():
         return render_template('index.html')
     except Exception as e:
         return str(e)
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    try:
+        mangae_db = Manage_db("Sampledatabase")
+        client = mangae_db.get_client()
+        db = client["Sampledatabase"]
+        collection = db["RawData"]
+        data = list(collection.find({}, {"_id": 0}))
+        return jsonify(data)
+    
+    except Exception as e:
+        return str(e)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
